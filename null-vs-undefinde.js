5504153efe51e03c2a7210fe32740c519adf1b75
Nayem1315 <ndeveloper315@gmail.com>
2021-02-03 19:51:48 +0600

    null vs undefiend

---
+++ null-vs-undefinde.js
@@ -0,0 +1,12 @@
+// let pakhi;
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return 
+// }
+// function add(num1, num2) {
+//     console.log(num1, num2);
+// }
+// const chor = { name: "smart chor", phone: 514541541 };
+// console.log(chor.phone);
+let fun = undefined;
+console.log(fun);
---
+++ true-false.js
@@ -0,0 +1,7 @@
+const name = "nayem"
+if (name.length) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
